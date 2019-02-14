894f364846e18d6fcbdfb559d4c942e074076cb4
mirocow <Mirocow@users.noreply.github.com>
2019-02-14 22:37:56 +0300

    Added dataContent

--- Widget.php
+++ Widget.php
@@ -105,42 +105,60 @@ class Widget extends \yii\base\Widget
             'please select' => self::t('widget', 'Please select one or more items from the list.'),
         ];
 
+        $csrfParam = \Yii::$app->request->csrfParam;
+        $csrfToken = \Yii::$app->request->getCsrfToken();
+
         $js =<<<JS
 function gridBulkActions(self, grid) {
     var ids = $(grid).yiiGridView('getSelectedRows'),
         options = self.options[self.selectedIndex],
         dataConfirm = options.getAttribute('data-confirm'),
         dataModal = options.getAttribute('data-modal'),
-        dataContent = options.getAttribute('modal-content') || 'modal-content',
+        dataContent = options.getAttribute('modal-content') || 'modal-content' || 'modal-body',
         url = options.getAttribute('url');
 
     if (!ids.length) {
-        alert('"{$texts['please select']}"');
+        alert('{$texts['please select']}');
         self.value = '';
     } else if (dataConfirm && !confirm(dataConfirm)) {
         self.value = '';
         return;
     } else if (dataModal) {
         var modal = $("#" + dataModal),
-            form = modal.find("." + dataContent + " form");
-        self.value = '';
-        $.each(ids, function(index, id) {
-            form.append('<input type=\"hidden\" name=\"' + (options.getAttribute('name') ? options.getAttribute('name') : 'ids') + '[]\" value=' + id + ' />');
-        });           
-        modal.modal("show"); 
-    } else if (url) {
-        var form = $('<form action=' + url + ' method=\"POST\"></form>'),
-            csrfParam = $('meta[name=csrf-param]').prop('content'),
-            csrfToken = $('meta[name=csrf-token]').prop('content');
-
-        if (csrfParam) {
-            form.append('<input type=\"hidden\" name=' + csrfParam + ' value=' + csrfToken + ' />');
+            form = modal.find("form." + dataContent);
+        if(url){
+            form.attr('action', url);
         }
-        
+        form.find("input[type='hidden']").remove();
+        form.append('<input type="hidden" name="{$csrfParam}" value="{$csrfToken}" />');        
         $.each(ids, function(index, id) {
-            form.append('<input type=\"hidden\" name=\"' + (options.getAttribute('name') ? options.getAttribute('name') : 'ids') + '[]\" value=' + id + ' />');
+            form.append('<input type="hidden" name="' + (options.getAttribute('name') ? options.getAttribute('name') : 'ids') + '[]" value=' + id + ' />');
+        });           
+        modal.modal("show");
+    } else if (dataContent) {
+        var content = $("." + dataContent),
+            form = content.find("form");
+        if(url){
+            form.attr('action', url);
+        }
+        form.find("input[type='hidden']").remove();            
+        form.append('<input type="hidden" name="{$csrfParam}" value="{$csrfToken}" />');
+        $.each(ids, function(index, id) {
+            form.append('<input type="hidden" name="' + (options.getAttribute('name') ? options.getAttribute('name') : 'ids') + '[]" value=' + id + ' />');
         });
-        
+        content.removeClass('hide');
+        content.find(".cancel").on( "click", function() {
+            self.value = '';
+            content.addClass('hide');
+        });        
+        content.on("click.cancel");
+    } else if (url) {
+        var form = $('<form action=' + url + ' method="POST"></form>');
+
+        form.append('<input type="hidden" name="{$csrfParam}" value="{$csrfToken}" />');
+        $.each(ids, function(index, id) {
+            form.append('<input type="hidden" name="' + (options.getAttribute('name') ? options.getAttribute('name') : 'ids') + '[]" value=' + id + ' />');
+        });
         form.appendTo('body').submit();
     }
 }        
